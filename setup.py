6e48393430de545493d765e037434a88dff6eb16
netra212 <netra200021kcbdr@gmail.com>
2025-06-27 13:07:43 +0100

    Setting setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import find_packages, setup
 
+setup(
+    name = "e-commerce-bot",
+    version = "0.0.1",
+    author = "netrakc",
+    author_email = "netra200021kcbdr@gmail.com",
+    packages = find_packages(),
+    install_requires = ['langchain-astradb', 'langchain']
+)
