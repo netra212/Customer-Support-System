22080f96dcfbb9d779182d71d4a3ec942ae74c8d
netra212 <netra200021kcbdr@gmail.com>
2025-06-27 13:06:11 +0100

    Data ingestion

---
+++ data_ingestion/data_ingestion.py
@@ -0,0 +1,15 @@
+from langchain_astradb import AstraDBGraphVectorStore
+from dotenv import load_dotenv
+import os
+import pandas as pd
+# from data_ingestion.data_transform import data_converter
+
+class ingest_data:
+    def __init__(self):
+        print("Data Ingestion Class Initialized...!")
+
+    def data_ingestion(self):
+        pass
+
+if __name__ == "__main__":
+    d_in = ingest_data()
